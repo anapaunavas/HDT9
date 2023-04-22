5ec0c56e7bd63b743795ab250168a3a151f9890b
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-04-22 00:11:17 -0500

    splay tree

--- SplayTree.java
+++ SplayTree.java
@@ -1,3 +1,20 @@
+    // Ana Paula Hong 22731
 public class SplayTree {
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class SplayTree <K, V> implements ISplayTree<K, V> {
+    private int count;
+	private treeNode<K, V> root;
+	private Comparator<K> keyComparator;
     
+    public BinarySearchTree(Comparator<K> _keyComparator) {
+		this.keyComparator = _keyComparator;
+		root = null;
+		count = 0;
+	}
+
+}
 }
